bbb559d2cd0875b4b595f8440ba6353c067a71f2
Zack Borges <zackartborges@gmail.com>
2023-04-28 11:00:01 -0400

    adds get comments/:id and async handling

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/outfit", (req, res) => {
     const tops = ["Black", "White", "Orange", "Navy"]
     const bottoms = ["Grey", "White", "Yellow", "Blue"]
@@ -17,10 +19,34 @@ app.get("/outfit", (req, res) => {
     });
 })
 
-app.post("/comments", (req, res) => {
+app.get("/comments/:id", async (req, res) => {
+    const id = req.params.id;
+    let content;
+
+    try {
+        content = await fs.readFile(`data/comments/${id}.txt`, "utf-8")
+    } catch (err) {
+        return res.sendStatus(404)
+    }
+
+    res.json({
+        content: content
+    })
+});
+
+app.post("/comments", async (req, res) => {
     const id = uuidv4();
-    console.log(id);
-    res.sendStatus(201)
+    const content = req.body.content;
+
+    if (!content) {
+        return res.sendStatus(400)
+    }
+
+    await fs.mkdir("data/comments", { recursive: true })
+    await fs.writeFile(`data/comments/${id}.txt`, content)
+    res.status(201).json({
+        id: id
+    })
 })
 app.listen(3000, () => {
     console.log('Server started');
