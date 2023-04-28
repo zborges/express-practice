8b3a763fe4f6817741322a54e5a245ad23c5180d
Zack Borges <zackartborges@gmail.com>
2023-04-27 21:52:49 -0400

    adds post request example

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
+app.get("/outfit", (req, res) => {
+    const tops = ["Black", "White", "Orange", "Navy"]
+    const bottoms = ["Grey", "White", "Yellow", "Blue"]
+    const shoes = ["White", "Orange", "Navy", "Black"]
+    res.json({
+        top: _.sample(tops),
+        bottoms: _.sample(bottoms),
+        shoes: _.sample(shoes)
+    });
+})
+
+app.post("/comments", (req, res) => {
+    const id = uuidv4();
+    console.log(id);
+    res.sendStatus(201)
+})
 app.listen(3000, () => {
     console.log('Server started');
 });
